5a376a5472ca00240b6843b7ec42f6ab90fe9942
Alexxfrolov <alex99-999@yandex.ru>
2019-11-20 11:53:26 +0300

    update root page

--- app/controllers/root.rb
+++ app/controllers/root.rb
@@ -2,8 +2,8 @@
 
 class Root < BaseController
   def index
-    @landings = App.router.routes[:get].
-      select {|i| i.last[:klass] == "Landing"}.map(&:first)
+    @landings = App.router.routes[:get]
+                   .select { |i| i.last[:klass] == 'Landing' }.map(&:first)
     render :index
   end
 end
