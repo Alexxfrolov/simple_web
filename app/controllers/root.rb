19fa63dd2fefcfbee1ab9fe9fc143144d30497b3
Alexxfrolov <alex99-999@yandex.ru>
2019-11-21 20:47:13 +0300

    route for clever on index page

--- app/controllers/root.rb
+++ app/controllers/root.rb
@@ -4,6 +4,8 @@ class Root < BaseController
   def index
     @landings = App.router.routes[:get]
                    .select { |i| i.last[:klass] == 'Landing' }.map(&:first)
+    @projects = App.router.routes[:get]
+                   .select { |i| i.last[:klass] == 'Clever' }.map(&:first)
     render :index
   end
 end
